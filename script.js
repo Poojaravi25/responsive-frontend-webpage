ac078c12e1bfb87c0eae0513b852ea32746871b8
Poojaravi25 <155242530+Poojaravi25@users.noreply.github.com>
2024-01-06 08:52:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const searchIcon=document.querySelector('.search-icon');
+const searchForm=document.querySelector('.search-form');
+
+searchIcon.addEventListener("click", ()=> {
+    searchForm.classList.add("active");
+    cartItemsContainer.classList.remove("active");
+    navbar.classList.remove("active");
+});
+
+
+const menuIcon=document.querySelector('.menu-icon');
+const navbar=document.querySelector('.navbar');
+
+menuIcon.addEventListener("click", () => {
+    navbar.classList.add("active");
+    searchForm.classList.remove("active");
+    cartItemsContainer.classList.remove("active");
+});
+
+
+
+const cartIcon=document.querySelector('.cart-icon');
+const cartItemsContainer=document.querySelector('.cart-items-container');
+
+cartIcon.addEventListener("click", ()=> {
+    cartItemsContainer.classList.add("active");
+    searchForm.classList.remove("active");
+    navbar.classList.remove("active");
+});
+
+window.onscroll = ()=>{
+    cartItemsContainer.classList.remove("active");
+    searchForm.classList.remove("active");
+    navbar.classList.remove("active");
+};
